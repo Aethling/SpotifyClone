72722fac2082e7e699297c5a5703cfd4eb47c4d5
Aric <aessig32@hotmail.com>
2019-02-14 12:38:38 -0700

    store added

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import reducer from './reducers/beer';
-// import { createStore } from 'redux';
-import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />	
+  </Provider>, document.getElementById('app')
+);
