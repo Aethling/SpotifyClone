4d2a90c6b1ab5300a1a20c8bd9d8f9f166cb6778
Aric Essig <aricessig@Arics-Macbook.local>
2019-03-24 13:28:22 -0600

    working on songControls timer

--- client/src/components/songControls/SongControls.js
+++ client/src/components/songControls/SongControls.js
@@ -3,13 +3,35 @@ import React, { Component } from 'react';
 class SongControls extends Component {
     state = {
         elapsedTime: 0
-    }
+		}
+		componentDidUpdate(prevProps) {
+			// if (prevProps.elapsedTime === 0) {
+			// 	this.calculateTime();
+			// 	console.log("woopee")
+			// }
+			console.log(this.state)
+			// this.calculateTime()
+		}
+		
+		calculateTime() {
+			const intervalID = setInterval(() => {
+				if (this.state.elapsedTime === 30) {
+					clearInterval(this.state.intervalID);
+				} else {
+					console.log(this.state.elapsedTime + 1)
+					this.setState({
+						elapsedTime: this.state.elapasedTime + 1
+					})
+				}
+			}
+			)
+		}
     render() {
         return (
 					<div className='song-progress-container'>
 						<p className='timer-start'></p>
 						<div className='song-progress'>
-							<div className='song-expired'></div>
+							<div className='song-expired'style={{width: this.state.elapsedTime}}></div>
 						</div>
 						<p className='timer-end'></p>
 					</div>
